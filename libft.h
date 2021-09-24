d36f420023314624eaad40a7c599a96da5bf5dcd
Cesar Menjivar <cemenjiv@student.42quebec.com>
2021-09-24 13:11:13 -0400

    Test3

---
+++ libft.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cemenjiv <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/24 13:04:42 by cemenjiv          #+#    #+#             */
+/*   Updated: 2021/09/24 13:08:05 by cemenjiv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+//Libft library containing the prototypes of all the functions created
+
+//Prototype of the "is" function
+
+
+
+
+
+//Prototype of the "str" function 
+
+
+
+
+
+
+//Prototype of the "mem" functions
+
+
+
+
+
+
+
+//Prototype of other functions
