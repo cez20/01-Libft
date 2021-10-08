b1bfad49451706c7fd006f70dcd6ce9722ab9151
Cesar Menjivar <cemenjiv@student.42quebec.com>
2021-10-08 12:16:25 -0400

    Update libft.h file so norminette valid

--- libft.h
+++ libft.h
@@ -6,60 +6,49 @@
 /*   By: cemenjiv <cemenjiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 13:04:42 by cemenjiv          #+#    #+#             */
-/*   Updated: 2021/10/06 16:21:36 by cemenjiv         ###   ########.fr       */
+/*   Updated: 2021/10/08 12:14:14 by cemenjiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+# include <stdlib.h
+# include <unistd.h>
+# include <string.h>
 
-//Prototype of the "is" function
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-
-//Prototype of the "str" function 
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-int	ft_strncmp(const char *s1,const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-size_t	ft_strlcat(char *dst, const char *src, size_t size); 
-
-//Prototype of the "mem" functions
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
-
-//Prototype of other functions
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	ft_bzero(void *s, size_t n);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-int	ft_atoi(const char *str);
-
-//Prototype 2 fonctions avec malloc
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
- 
- //Partie 2
- char	*ft_substr(char const *s, unsigned int start, size_t len);
- char	*ft_strjoin(char const *s1, char const *s2);
- char	*ft_strtrim(char const *s1, char const *set);
- char	*ft_itoa(int n);
- char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
- void	ft_striteri(char *s, void (*f)(unsigned int, char*));
- void	ft_putchar_fd(char c, int fd);
- void	ft_putstr_fd(char *s, int fd);
- void	ft_putendl_fd(char *s, int fd);
- void	ft_putnbr_fd(int n, int fd);
- 
- #endif
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
+#endif
