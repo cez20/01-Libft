0670ea64e282fcdc51cbaa99872f40360e0d4555
Cesar Menjivar <cemenjiv@student.42quebec.com>
2021-10-25 17:08:16 -0400

    9

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: cemenjiv <cemenjiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 10:43:26 by cemenjiv          #+#    #+#             */
-/*   Updated: 2021/10/08 11:54:12 by cemenjiv         ###   ########.fr       */
+/*   Updated: 2021/10/25 16:45:08 by cemenjiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
+	if ((start >= ft_strlen(s)) || (len == 0))
+		return (ft_calloc(1, sizeof(char)));
+	if (len > (ft_strlen(s) - start))
+		len = ft_strlen(s) - start;
 	str = (char *)malloc((len + 1) * sizeof(*str));
 	if (!str)
 		return (NULL);
@@ -28,10 +32,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	while (s[i] != '\0')
 	{
 		if (i >= start && j < len)
-		{
-			str[j] = s[i];
-			j++;
-		}
+			str[j++] = s[i];
 		i++;
 	}
 	str[j] = '\0';
